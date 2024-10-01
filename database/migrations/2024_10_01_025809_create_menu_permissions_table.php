cd50c5a51bd6f6c759666d4f08f8e610111d0992
ekahanifr <ekahanifr@gmail.com>
2024-10-01 10:17:43 +0700

    base table

---
+++ database/migrations/2024_10_01_025809_create_menu_permissions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('role_id');
+            $table->integer('menu_id');
+            $table->boolean('granted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu_permissions');
+    }
+};
