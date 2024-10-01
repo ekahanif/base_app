894427684f15ad1a8e7cce07851b4db155c48d99
ekahanifr <ekahanifr@gmail.com>
2024-10-01 11:22:59 +0700

    register role user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,23 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 class AuthController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|max:255|unique:users',
             'password' => 'required|string|min:8',
         ]);
+        
 
         if ($validator->fails()) {
             return response()->json($validator->errors());
-        }
-        else{
+        } else {
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
@@ -28,7 +30,17 @@ class AuthController extends Controller
                 'password' => Hash::make($request->password),
             ]);
 
-            $token = $user->createToken('auth_token')->plainTexToken;
+            if ($request->role != 1) {
+                $userProfile = UserProfile::create([
+                    'user_id' => $user->id,
+                    'gender' => $request->gender,
+                    'birth_date' => $request->birth_date,
+                    'phone' => $request->phone,
+                    'address' => $request->address,
+                ]);
+            }
+
+            $token = $user->createToken('auth_token')->plainTextToken;
 
             return response()->json([
                 'data' => $user,
@@ -39,7 +51,8 @@ class AuthController extends Controller
     }
 
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json(['message' => 'Unauthorized'], 401);
         }
